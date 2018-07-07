357dca63d301027541a17910fc09a2378b54a708
Noah Wolfe <nmwolfe@umich.edu>
2018-07-06 22:40:54 -0400

    Added reported hashrate, current workers, and earnings

--- MiningMonitor/MainPage.xaml.cs
+++ MiningMonitor/MainPage.xaml.cs
@@ -14,19 +14,29 @@ namespace MiningMonitor
         public MainPage()
         {
             InitializeComponent();
+
+            refreshStats();
         }
+
         private void refreshButton_clicked(object sender, EventArgs e)
         {
-            //RootObject root = new RootObject();
+            refreshStats();
 
+        }
+
+        private void refreshStats() 
+        {
             string URL = "https://api.ethermine.org/miner/:0xcC61A3210F9feF03038eE100fA9Dd4a1dA8BfdC2/currentStats";
             using (var webClient = new System.Net.WebClient())
             {
                 var json = webClient.DownloadString(URL);
-                // Now parse with JSON.Net
+
                 RootObject miningOutput = JsonConvert.DeserializeObject<RootObject>(json);
 
-                hashrate.Text = "Current Hashrate: " + (Math.Round(miningOutput.data.currentHashrate/1000000, 2)).ToString() + " MH/s";
+                repHashrate.Text = "Reported Hashrate: " + (Math.Round((double)miningOutput.data.reportedHashrate / 1000000, 2)).ToString() + " MH/s";
+                curHashrate.Text = "Current Hashrate: " + (Math.Round(miningOutput.data.currentHashrate / 1000000, 2)).ToString() + " MH/s";
+                workers.Text = "Active Workers: " + miningOutput.data.activeWorkers;
+                earnings.Text = "Daily Earnings: $" + (Math.Round(miningOutput.data.usdPerMin * 60 * 24, 2)).ToString() + "/day";
             }
         }
     }
