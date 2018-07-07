bac1e9fbe141eb1fbc933dbb3f3597c5b358c844
Noah Wolfe <nmwolfe@umich.edu>
2018-07-06 22:20:49 -0400

    Initial MiningMonitor commit, basic app is working

---
+++ MiningMonitor/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+
+namespace MiningMonitor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void refreshButton_clicked(object sender, EventArgs e)
+        {
+            //RootObject root = new RootObject();
+
+            string URL = "https://api.ethermine.org/miner/:0xcC61A3210F9feF03038eE100fA9Dd4a1dA8BfdC2/currentStats";
+            using (var webClient = new System.Net.WebClient())
+            {
+                var json = webClient.DownloadString(URL);
+                // Now parse with JSON.Net
+                RootObject miningOutput = JsonConvert.DeserializeObject<RootObject>(json);
+
+                hashrate.Text = miningOutput.data.averageHashrate.ToString();
+            }
+        }
+    }
+}
---
+++ MiningMonitor/RootObject.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MiningMonitor
+{
+    public class RootObject
+    {
+        public string status { get; set; }
+        public Data data { get; set; }
+    }
+}
