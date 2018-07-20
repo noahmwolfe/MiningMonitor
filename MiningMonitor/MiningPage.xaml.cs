57a90e44bbd45e6fa00fd547c39d93411e7fda5f
Noah Wolfe <nmwolfe@umich.edu>
2018-07-20 08:46:22 -0500

    Added call to Siamining API. Need to figure out Mike.rs pool api for 0xbtc

--- MiningMonitor/MiningPage.xaml.cs
+++ MiningMonitor/MiningPage.xaml.cs
@@ -58,17 +58,17 @@ namespace MiningMonitor
 
         private void refreshsiaStats()
         {
-            string URL = "https://siamining.com/api/v1/addresses/:50285401fe38b97b22cde4ee344a0bea985d80175d30f973343ebb33e4b5892ca223a1c97f9c/summary";
+            string URL = "https://siamining.com/api/v1/addresses/50285401fe38b97b22cde4ee344a0bea985d80175d30f973343ebb33e4b5892ca223a1c97f9c/summary";
             using (var webClient = new System.Net.WebClient())
             {
                 var json = webClient.DownloadString(URL);
 
                 RootObject miningOutput = JsonConvert.DeserializeObject<RootObject>(json);
 
-                ethrepHashrate.Text = "Reported Hashrate: " + (Math.Round((double)miningOutput.data.reportedHashrate / 1000000, 2)).ToString() + " MH/s";
-                ethcurHashrate.Text = "Current Hashrate: " + (Math.Round(miningOutput.data.currentHashrate / 1000000, 2)).ToString() + " MH/s";
-                ethworkers.Text = "Active Workers: " + miningOutput.data.activeWorkers;
-                ethearnings.Text = "Daily Earnings: $" + (Math.Round(miningOutput.data.usdPerMin * 60 * 24, 2)).ToString() + "/day";
+                //ethrepHashrate.Text = "Reported Hashrate: " + (Math.Round((double)miningOutput.data.reportedHashrate / 1000000, 2)).ToString() + " MH/s";
+                siacurHashrate.Text = "Current Hashrate: " + (Math.Round(miningOutput.hashes / 1000000, 2)).ToString() + " MH/s";
+                ethworkers.Text = "Unpaid Balance: " + (Math.Round(double.Parse(miningOutput.balance), 2)).ToString() + " Sia";
+                siaearnings.Text = "Total Earnings: " + (Math.Round(double.Parse(miningOutput.paid), 2)).ToString() + " Sia";
             }
         }
     }
--- MiningMonitor/RootObject.cs
+++ MiningMonitor/RootObject.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace MiningMonitor
 {
     public class RootObject
     {
         public string status { get; set; }
         public Data data { get; set; }
+        public string address { get; set; }
+        public double hashes { get; set; }
+        public long valid_shares { get; set; }
+        public int stale_shares { get; set; }
+        public int invalid_shares { get; set; }
+        public int blocks_found { get; set; }
+        public string balance { get; set; }
+        public string paid { get; set; }
+        public int last_share_time { get; set; }
+        public List<object> intervals { get; set; }
     }
 }
